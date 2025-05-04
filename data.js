6560fe5a0480cd0be66dc6853bdc765dc15249c5
Mepaprog <147797715+Mepaprog@users.noreply.github.com>
2025-05-04 11:41:15 +0800

    Update data.js

--- data.js
+++ data.js
@@ -23,9 +23,12 @@ const data =
         "The stage where the artist creates the artwork, using various materials and techniques, and where the idea starts to take physical form.": "PRODUCTION",
         "once an artwork is complete, decisions are made about how it will be shared with the public, often through museums, galleries, or performance spaces. The artist hopes the audience will connect with the unique message embedded in the work.": "POSTPRODUCTION",
         "Once the artwork is finished, this stage involves deciding how to share and display it in public spaces like galleries or museums, with the hope that the audience connects with its message.": "Technique",
-
-
-
+        "serve as a bridge between art and the public, making art relevant and accessible to contemporary audiences.": "Exhibitions",
+        " are also a good way to introduce the artwork and opens it up for appreciation, critique, and analysis": "Publications",
+        "The highest national recognition given to Filipino artists who have made notable contributions to Philippine arts.": "The Order of National Artists ( Tagad ng Sining)",
+        " a painter and the very first recipient of this award who was touted as the 'Grand Old Man of Philippine Art' He was the sole awardee in the year 1972, a National Artist for Visual Arts": "Fernando Amorsolo",
+        "established in 1992, celebrates artists who excel in traditional Filipino arts, such as weaving, pottery, and music. The award supports the preservation and teaching of these traditional skills.": "The National Living Treasures Award (Gawad sa Manlilikha ng Bayan)",
+        "THE THREE OUTSTANDING ARTISTS IN MUSIC AND POETRY (1993)": "1. Ginaw Bilog<br>2. Masino Intaray<br>3. Samaon Sulaiman",
 
         "Is a term used to describe a period of history when stone were used to make tools for survival": "Stone Age",
         "Is a term image of men and women dressed in skin, huddling before a fire in a cave": "Conjures",
@@ -33,7 +36,19 @@ const data =
         "Middle stone age": "MESOLITHIC",
         "New stone age": "NEOLITHIC",
         "Three periods of stone age": "PALEOLITHIC <br> MESOLITHIC <br> NEOLITHIC",
-        "Three period of egyptian civilization": "old <br> Middle <br> New Kingdom"
+        "is the earliest form of art, created by early humans in caves during the Ice Age as they sought shelter and warmth, leading them to make drawings and carvings on cave walls.": "Paleolithic Art",
+        "is art from the Middle Stone Age, showing daily life and hunting scenes as people began to settle and adapt to their surroundings.": "Mesolithic Art",
+        "is art from the New Stone Age, showing more advanced designs like pottery, weaving, and large stone structures, as people began farming and living in permanent homes.": "Neolithic Art",
+        "Prehistoric Arts": "Paleolithic Art<br>Mesolithic Art<br>Neolithic Art",
+        "Three period of egyptian civilization": "old <br> Middle <br> New Kingdom",
+        " It was a palette that utilized and applied dark colors around King Narmer’s eyes.": "NARMER PALETTE",
+        "is an ancient tomb built during Egypt's Old Kingdom to serve as the resting place for a pharaoh and is one of the world's great architectural wonders.": "The Great Pyramid Of Giza",
+        "was an Egyptian queen known for her famous bust, which shows realistic and graceful features, highlighting the natural beauty often used in royal art.": "Queen Nefertiti",
+        "was a young Egyptian pharaoh whose tomb, found in 1922, amazed the world with its gold treasures and famous gold mask.": "King Tutankhamen",
+
+
+
+
     },
     "Topic2": {
         "4+4": "8",
