c6a453cc144c8b71ff8de665c26bfbc5223626b9
Mepaprog <147797715+Mepaprog@users.noreply.github.com>
2025-05-04 10:55:40 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,32 @@
 const data =
 {
     "Art app Final": {
+        "He stated that 'Art when really understood is the province of every human being'": "Robert Henri",
+        "Are packed with numerus artifacts and interesting objects from all over the world that have survived centuries": "Museum",
+        " A skilled worker who creates handmade items with both skill and artistic design, like jewelry, pottery, or decorative objects.": "Artisan",
+        "were groups of skilled workers in the same trade who helped each other and set rules to keep their work high quality.": "Guilds",
+        "A skilled worker who focuses more on practical and functional items, like furniture or tools.": "Craftsmen",
+        "A person who draws detailed plans or technical drawings for buildings, machines, or other projects.": "Draftsman",
+        "A person who designs buildings and oversees how they are built.": "Architect",
+        "A person who uses science and math to design and build things like bridges, machines, or electrical systems.": "Engineer",
+        "A person who actually constructs or puts together buildings and structures.": "Builder",
+        "are all roles that were often combined into one person or group during the Middle Ages.": "draftsman<br> architect<br> engineer<br> builder",
+        "There were those whose work station were segmented into two": "studiolo and the bottega",
+        "is a person who creates artwork, and for their career to grow, their work must be recognized, valued, and appreciated by galleries, exhibitions, and collectors.": "Artist",
+        "is someone who supports and promotes an artist's work by spotting talent, helping build the artist's career, and gaining exposure to establish their reputation.": "Dealer",
+        "is an expert in art who organizes exhibitions at museums, galleries, or public spaces. They are responsible for planning the show, researching artists, and creating exhibition designs and catalogue essays.": "Curator",
+        "analyze and evaluate artwork, commenting on its aesthetics, technical aspects, and themes. They write reviews for magazines, newspapers, or websites, or work as freelancers.": "Art critics",
+        "are key figures in the art market, supporting artists, dealers, galleries, and auction houses. Their purchases can help recognize and promote unknown artists, and many artworks in museums are donated by collectors.": "Collectors",
+        "Players in the World of Art": "Artist<br>Dealer<br>Curator<br>Art Critics<br>Collectors",
+        "PRODUCTION PROCESS": "(1) preproduction<br>(2) production<br>(3) postproduction.",
+        "The stage where the artist develops and explores their idea, which may take time or come quickly through research and experimentation.": "PREPRODUCTION",
+        "The stage where the artist creates the artwork, using various materials and techniques, and where the idea starts to take physical form.": "PRODUCTION",
+        "once an artwork is complete, decisions are made about how it will be shared with the public, often through museums, galleries, or performance spaces. The artist hopes the audience will connect with the unique message embedded in the work.": "POSTPRODUCTION",
+        "Once the artwork is finished, this stage involves deciding how to share and display it in public spaces like galleries or museums, with the hope that the audience connects with its message.": "Technique",
+
+
+
+
         "Is a term used to describe a period of history when stone were used to make tools for survival": "Stone Age",
         "Is a term image of men and women dressed in skin, huddling before a fire in a cave": "Conjures",
         "The later years of the old stone age": "PALEOLITHIC",
