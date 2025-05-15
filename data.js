cc8b0cc269d449b61a85cd32fdd5a01aae262101
Mepaprog <147797715+Mepaprog@users.noreply.github.com>
2025-05-15 14:23:26 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const data =
 {
     "Art app Final": {
-        "Chapter 5": {
+        "Lesson 5": {
             "He stated that 'Art when really understood is the province of every human being'": "Robert Henri",
             "Are packed with numerus artifacts and interesting objects from all over the world that have survived centuries": "Museum",
-            " A skilled worker who creates handmade items with both skill and artistic design, like jewelry, pottery, or decorative objects.": "Artisan",
-            "were groups of skilled workers in the same trade who helped each other and set rules to keep their work high quality.": "Guilds",
+            "A skilled worker who creates handmade items with both skill and artistic design, like jewelry, pottery, or decorative objects.": "Artisan",
+            "were a type of social fellowship, prevalent during the middle ages, particularly the 13th century to 15th century, where towns had formalized groups of artisans or craftsmen who took on a particular specialization or trade: shoemakers, textile and glass workers, carpenters, carvers, masons armorers.": "Guilds",
             "A skilled worker who focuses more on practical and functional items, like furniture or tools.": "Craftsmen",
             "A person who draws detailed plans or technical drawings for buildings, machines, or other projects.": "Draftsman",
             "A person who designs buildings and oversees how they are built.": "Architect",
@@ -13,17 +13,19 @@ const data =
             "A person who actually constructs or puts together buildings and structures.": "Builder",
             "are all roles that were often combined into one person or group during the Middle Ages.": "draftsman<br> architect<br> engineer<br> builder",
             "There were those whose work station were segmented into two": "studiolo and the bottega",
-            "is a person who creates artwork, and for their career to grow, their work must be recognized, valued, and appreciated by galleries, exhibitions, and collectors.": "Artist",
-            "is someone who supports and promotes an artist's work by spotting talent, helping build the artist's career, and gaining exposure to establish their reputation.": "Dealer",
-            "is an expert in art who organizes exhibitions at museums, galleries, or public spaces. They are responsible for planning the show, researching artists, and creating exhibition designs and catalogue essays.": "Curator",
-            "analyze and evaluate artwork, commenting on its aesthetics, technical aspects, and themes. They write reviews for magazines, newspapers, or websites, or work as freelancers.": "Art critics",
-            "are key figures in the art market, supporting artists, dealers, galleries, and auction houses. Their purchases can help recognize and promote unknown artists, and many artworks in museums are donated by collectors.": "Collectors",
+            "Their beautiful vision and creative endeavors have to be recognized and appreciated to help build a career and open up opportunities. The artwork needs to be valued and receive serious consideration by galleries, exhibitions, biennials, and collectors alike.": "Artist",
+            "are essentially the supporters and promoters of an artist's work. Their primary role is to spot talent, nurture an artist's career, generate exposure and establish his/her reputation.": "Dealer",
+            "have an eye and passion for all art forms. They use their extensive knowledge of the subject and organizational skills to coordinate exhibitions at museums, galleries or public spaces. Curators are responsible for the conceptual development, design, layout and the execution of an art show. Art curators also research artists and write catalogue essays.": "Curator",
+            "They analyze, evaluate and interpret the aesthetic value and significance of artwork that is displayed at galleries, museum exhibitions, private or public showings. Critics also comment and pass judgement on the technical factors, theme and artistic expression of the art being reviewed.": "Art critics",
+            "They are an integral part of the art market that are keeping it afloat. The contributions of art collectors are vital for the very existence of artists, dealers, galleries and auction houses. Prominent collectors are very influent and can create recognition of an unknown artist by purchasing his work.": "Collectors",
             "Players in the World of Art": "Artist<br>Dealer<br>Curator<br>Art Critics<br>Collectors",
             "PRODUCTION PROCESS": "(1) preproduction<br>(2) production<br>(3) postproduction.",
             "The stage where the artist develops and explores their idea, which may take time or come quickly through research and experimentation.": "PREPRODUCTION",
             "The stage where the artist creates the artwork, using various materials and techniques, and where the idea starts to take physical form.": "PRODUCTION",
             "once an artwork is complete, decisions are made about how it will be shared with the public, often through museums, galleries, or performance spaces. The artist hopes the audience will connect with the unique message embedded in the work.": "POSTPRODUCTION",
-            "Once the artwork is finished, this stage involves deciding how to share and display it in public spaces like galleries or museums, with the hope that the audience connects with its message.": "Technique",
+            "is the mode of expression in which the concept, idea, or message is conveyed. It may be physical, such as paintings, sculptures, monuments, and structures; or it may even be ephemeral or transient, such as a track (sound recording), a film, or a performance.": "MEDIUM ",
+            "shows the level of familiarity with the medium being manipulated, it suggests the necessity of additional tools or implements, or consideration of time, and specificity of the site of creation.": "Technique",
+            "evolved over time, especially in how exhibitions are organized and presented.": "ENGAGEMENT WITH ART",
             "serve as a bridge between art and the public, making art relevant and accessible to contemporary audiences.": "Exhibitions",
             " are also a good way to introduce the artwork and opens it up for appreciation, critique, and analysis": "Publications",
             "The highest national recognition given to Filipino artists who have made notable contributions to Philippine arts.": "The Order of National Artists ( Tagad ng Sining)",
@@ -32,7 +34,7 @@ const data =
             "THE THREE OUTSTANDING ARTISTS IN MUSIC AND POETRY (1993)": "1. Ginaw Bilog<br>2. Masino Intaray<br>3. Samaon Sulaiman"
         },
 
-        "Chapter 7": {
+        "Lesson 7": {
             "Is a term used to describe a period of history when stone were used to make tools for survival": "Stone Age",
             "Is a term image of men and women dressed in skin, huddling before a fire in a cave": "Conjures",
             "The later years of the old stone age": "PALEOLITHIC",
@@ -50,30 +52,38 @@ const data =
             "was a young Egyptian pharaoh whose tomb, found in 1922, amazed the world with its gold treasures and famous gold mask.": "King Tutankhamen"
         },
 
-        "Chapter 8": {
-            "Four Periods of Greek Arts": "ARCHAIC PERIOD<br>CLASSICAL PERIOD<br>HELLENISTIC PERIOD<br>GEOMETRIC PERIOD",
-            "Is religious art with icons, mosaics, and frescoes in domed churches, known for rich colors and gold backgrounds.": "Byzantine Art",
-            "Is art inspired by ancient Greece and Rome, with improved techniques and more focus on realistic people and nature.": "Renaissance Art",
-            "Is vigorous and direct, was originally brightly colored, and is often very sophisticated": "Romanesque Art",
-            "Is an art style that used distorted shapes and space to show emotion, reacting against the balance and perfection of earlier Renaissance art.": "Mannerism",
-            "Is an art style from the Enlightenment that focused on reason, order, and classical themes from ancient Greece and Rome.": "Neoclassicism",
-            "Is an art style that focused on emotion, nature, and the goodness of people, promoting justice, equality, and strong feelings.": "Romanticism",
-            "Is an art style that shows life as it really is, with accurate details and everyday subjects, breaking away from traditional idealized art.": "Realism",
-            "Is a painting style from France that used light and color to capture quick, everyday moments, breaking from traditional art.": "Impressionism",
-            "Is an art style that grew from Impressionism but added more structure, emotion, and personal expression.": "Post-Impressionism",
-            "Is an art style that uses scientific techniques, like dots of color, to create specific effects and influence how viewers see the artwork.": "Neo-Impressionism",
-            "Is an ornamental art style from 1890 to 1910 that broke away from traditional, historical designs with flowing, natural forms.": "Art Nouveau",
-            "Is a painting style from France that used bold, vibrant colors directly from the paint tubes, creating a strong, emotional effect.": "Fauvism",
-            "Is an art style that focuses on flat, two-dimensional surfaces, breaking away from traditional techniques like perspective and modeling to show multiple views of objects at once.": "Cubism",
-            "Is an art movement from Italy in the early 20th century that focused on speed, energy, and the power of machines.": "Futurism"
+        "Lesson 8": {
+            "stands out among that other ancient culture for its development of naturalistic but idealized depictions of the human body, in which largely nude male figures were generally the focus of innovation.": "Ancient Greece art",
+            "FOUR PERIODS OF GREEK ARTS": "Archaic Period<br>Classical Art<br>Hellenistic Period<br>Geometric Period",
+            "the period between the decline of the Roman Empire and the Renaissance. It was a period that is characterized by ignorance and darkness. The printing press came later after the Middle Ages, so copies of the scriptures were done by hand. ": "MIDDLE AGES",
+            "Roman art was influenced by Greece and can in part be taken as a descendant of ancient Greek painting and sculpture, but was also strongly influenced by the more local Etruscan art of Italy.": "ANCIENT ROME",
+            "a general term for the art of the barbarian peoples who moved into formerly Roman territories. Celtic art in the 7th and 8th centuries saw a fusion with Germanic traditions through contact with the Anglo-Saxons creating what is called the Hiberno-Saxon style or Insular art, which was to be highly influential on the rest of the Middle Ages.": "EARLY MEDIEVAL ART/MIGRATION PERIOD ART",
+            "took the representationalism of the Renaissance to new heights, emphasizing detail, movement, lighting, and drama in their search for beauty. Was characterized by strongly religious and political themes; common characteristics included rich colors with a strong light and dark contrast.": "Baroque art",
+            "was characterized by lighter, often jocular themes; common characteristics included pale, creamy colors, florid decorations and a penchant for bucolic landscapes. ": "ROCOCO ART",
+            "was the artistic component of the intellectual movement known as the Enlightenment; the Enlightenment was idealistic, and put it in emphasis on objectivity, reason and empirical truth.": "NEOCLASSICISM",
+            "The emphasis on the goodness of mankind. Most works also promoted justice, equality and social order. Artists also emphasized emotions and feelings of man, which was a deviation from the humanist principles of rationalism.": "ROMANTICISM",
+            "focuses on the accuracy of details that depicts and somehow mirrors reality. Realism as a modern movement in art veered away from traditional forms of art.": "REALISM",
+            "The Impressionism movement started in France, which led to a break from the tradition in European painting.  Impressionism is a style of painting that emerged in the mid-to late 1800s.": "IMPRESSIONISM",
+            "It is an art movement that emerged in France, which is a result of both the influence and rejection of Impressionism. Most artists that belong to this movement started off as impressionists but later on saw the inherent limitations and flaws of impressionism.": "POST-IMPRESSIONISM",
+            "As an art movement, neo-impressionism is considered as a response to empirical realism of impressionism. Most painters who subscribe to such movement rely on a systematic and scientific techniques that have a predetermined visual effects not only on the art work itself but also how the audience perceive the art.": "NEW-IMPRESSIONISM",
+            "Between 1890 and 1910, countries from Europe and the United States witnessed the emergence and flourishing of a new art style. This ornamental style of art was a break from the Conservative historicism, which was the prevailing and dominant theme of most Western artworks.": "ART NOUVEAU",
+            "This is a style of painting that emerged in France around the turn of the twentieth century. What makes fauvists revolutionary is that they used pure and vibrant colors by applying straight from the paint tubes directly to the canvas.": "FAUVISM",
+            "This style would later on have a huge influence on artists during the twentieth century. Cubists highlighted the two dimensional surface of the picture plane. Focusing on a flat surface was a rejection of the dominant techniques like the use of perspective, foreshortening, and modeling.": "CUBISM",
+            "It is an early twentieth century art movement that started in Italy, which highlighted the speed, energy, dynamism and power of machines.": "FUTURISM"
         },
 
-        "Chapter 9": {
+        "Lesson 9": {
             "Is an art movement from the 1980s that revived the style of Pop Art, especially inspired by Andy Warhol and his peers. It mixed popular culture with the use of ready-made objects, similar to the earlier Dada movement. Neo-Pop artworks often looked like photos because of their clean, precise style, but without showing the artist’s personal touch.": "Neo-Pop Art",
             "Is a type of modern art where the artist transforms a space using various objects, materials, lights, sounds, or even scents, creating an immersive experience for viewers. It is often large-scale, site-specific, and sometimes temporary.": "Installation Art",
             "Is a style of art where the artist creates very detailed and realistic images, often based on photographs, making the artwork look almost like a real photo.": "Photorealism",
             "Is a type of art where artists shape or change natural landscapes using materials like rocks, soil, or plants, creating large outdoor works that blend with nature.": "Earth Art",
             "Is a form of public art that includes murals, stencils, stickers, and installations, often made in urban spaces using different styles and materials. It grew from graffiti and is meant to be seen by everyone in public places.": "Street Art"
+        },
+        "Lesson 10": {
+            "refers to the distinctive handling of elements and media associated with the work of an individual artist, a school, a movement, or a specific culture or time period (Fichner-Rathus, 2013).": "Style",
+            "is what the audience sees a finished product put harmoniously (or not) according to the different principles of design. In essence, form is the totality of the artwork, which includes the textures, colors, and shapes utilized by the artist.": "Form",
+            "of an artwork includes not only its form but also its subject matter and underlying meanings or themes (Fichner-Rathus, 2013).": "Content",
+            "can be defined as doing something without prior preparation. Within the present context, improvisation has become an integral part of the arts.": "Improvisation"
         }
 
         
